5cf2380529c3ec14f802fb85556d9d1e08f5decc
RahmatH07-lab <rh200107@gmail.com>
2024-12-02 13:45:06 +0700

    Create script-slider.js

---
+++ script-slider.js
@@ -0,0 +1,3 @@
+// Tidak diperlukan script tambahan karena animasi sudah diatur dengan CSS.
+// Namun, jika ingin membuat slider lebih dinamis, tambahkan logika di sini.
+console.log("Slider aktif!")
